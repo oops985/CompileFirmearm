a35e10f78d84c64c28c6c5d1a697f555c3e23aa8
oops985 <60307819+oops985@users.noreply.github.com>
2023-09-16 09:11:57 +0800

    Update 1.clone.sh

--- preset-lede/1.clone.sh
+++ preset-lede/1.clone.sh
@@ -10,4 +10,4 @@ git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
 git clone --depth 1 https://github.com/kenzok8/openwrt-packages.git
 git clone --depth 1 https://github.com/kenzok8/small.git
-git clone --depth 1 https://github.com/Ausaci/luci-app-nat6-helper.git
+#git clone --depth 1 https://github.com/Ausaci/luci-app-nat6-helper.git
